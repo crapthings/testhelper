110698d89ef88006dd4ebb8b545ecb698e236d7b
crapthings <crapthings@gmail.com>
2020-09-25 14:20:25 +0800

    minor update

--- server/main.js
+++ server/main.js
@@ -1,3 +1,14 @@
-console.time()
-Activities.find({}, { limit: 1000 }).fetch()
-console.timeEnd()
+// console.time()
+// Activities.find({}, { limit: 100000 }).fetch()
+// console.timeEnd()
+
+(async function () {
+  console.time('meteor')
+  Activities.find({}).fetch()
+  console.timeEnd('meteor')
+
+  console.time('raw')
+  // console.log(await Activities.rawCollection().find().toArray())
+  await Activities.rawCollection().find().toArray()
+  console.timeEnd('raw')
+})()
